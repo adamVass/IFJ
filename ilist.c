74548d90ccc5104be3f5f552b83e37590cb49af9
B3rs3k3r <B3rs3k3r@gmail.com>
2014-11-22 17:49:35 +0100

    Added doxy comments

--- ilist.c
+++ ilist.c
@@ -2,19 +2,31 @@
  * Soubor:  ilist.c
  * Autor:   Adam Vass, xvassa00@stud.fit.vutbr.cz
  * Projekt: IFJ; zoznam istrukcii
- */
+
+*******************************************************************/
+/**
+*@file ilist.c
+*
+*@brief Instruction list (singly linked list) implementation
+*@author Adam Vass (xvassa00)
+*/
 
 #include "ilist.h"
 
+#include <stdlib.h>
+#include <stdio.h>
 
 void ErrReport()
-{
+{ /** General error reporting function */
 	printf("Illegal operation within instruction list\n");
 	return;
 }
 
 void InitList (tList *L)
-{
+{ 
+/** Initializes list
+* @param *L Linked list
+*/
 	L->First = NULL;
 	L->Last = NULL;
 	L->Active = NULL;
@@ -25,9 +37,9 @@ void DisposeList (tList *L)
 	L->Active = L->First;
 	while(L->Active != NULL)
 	{
-		L->First = L->Active->nextItem; 	/* Nastaví nasledujúci prvok ako prvý */
-		free(L->Active); 					/* Uvoľní aktívny prvok */
-		L->Active = L->First; 				/* Nastaví ďalší (po novom prvý) prvok ako aktuálny */
+		L->First = L->Active->nextItem; 	/* Sets following item as first */
+		free(L->Active); 					/* Frees active item */
+		L->Active = L->First; 				/* Sets next item as active */
 	}
 
 	L->Last = NULL;
@@ -35,6 +47,10 @@ void DisposeList (tList *L)
 
 void InsertFirst (tList *L, tInst instr)
 {
+/** Inserts new item to the beginning of list.
+* @param *L Linked list
+* @param instr Instruction
+*/
 	tItemPtr newFirst = NULL;
 	if ((newFirst = malloc(sizeof(struct tItem))) == NULL) /* Ak alokácia zlyhá volá ErrReport() a končí */
 	{
@@ -43,14 +59,18 @@ void InsertFirst (tList *L, tInst instr)
 	}
 
 	newFirst->instruction = instr;
-	newFirst->nextItem = L->First; 		/* Nový prvý prvok zoznamu bude ukazovať na pôvodný prvý prvok zoznamu */
+	newFirst->nextItem = L->First; 		/* New first item of the list will be pointing to original first item */
 	if (L->First == NULL)
-		L->Last = newFirst; 			/* Ak je zoznam prázdny nový prvý prvok bude zároveň posledný */
-	L->First = newFirst; 				/* Nový prvok zoznamu bude nastavený ako začiatočný */
+		L->Last = newFirst; 			/* If the list is empty, new first element will be last as well */
+	L->First = newFirst; 				/* Newly added item will be set as first */
 }
 
 void InsertLast(tList *L, tInst instr)
 {
+/** Inserts new item to the end of list.
+* @param *L Linked list
+* @param instr Instruction
+*/
 	tItemPtr newLast = NULL;
 	if ((newLast = malloc(sizeof(struct tItem))) == NULL)
 	{
@@ -62,7 +82,7 @@ void InsertLast(tList *L, tInst instr)
 	newLast->nextItem = NULL;
 
 	if (L->First == NULL)
-		L->First = newLast; /* Ak je zoznam prázdny nový posledný prvok bude zároveň prvý */
+		L->First = newLast; /* If the list is empty, new last element will be first as well */
 	else
 		L->Last->nextItem = newLast;
 
@@ -71,41 +91,51 @@ void InsertLast(tList *L, tInst instr)
 
 void DeleteFirst (tList *L)
 {
+/** Deletes first item in the list, frees allocated memory.
+* @param *L Linked list
+*/
 	if (L->First != NULL)
 	{
-	tItemPtr temp = L->First; 			/* Do pomocnej premennej ukladá adresu prvého prvku zoznamu */
+	tItemPtr temp = L->First; 			/* Stores address of first element into variable */
 
 	if (L->Active == L->First)
-		L->Active = NULL; 				/* Ak je prvý prvok zároveň aktívny, aktivita sa stráca */
-		L->First = L->First->nextItem; 	/* Prvým prvkom sa stáva nasledujúci prvok */
-		free(temp); 					/* Pôvodný prvý prvok zaniká, pamäť je vrátená OS */
+		L->Active = NULL; 				/* If activity is on the first item, it is nulled */
+		L->First = L->First->nextItem; 	/* Following item in the list becomes first */
+		free(temp); 					/* Original item is freed, allocated memory returned to the OS */
 	}
 }
 
 void PostDelete (tList *L)
 {
+/** Deletes item following active one.
+* @param *L Linked list.
+*/
 	if ((L->Active != NULL) && (L->Active->nextItem != NULL))
 	{
-		tItemPtr temp = L->Active->nextItem; 		/* Do pomocnej premennej vloží prvok zoznamu za akt. prvkom */
-		L->Active->nextItem = temp->nextItem; 		/* Ako nasledujúci prvok nastaví prvok za tým, kt. bude vymazaný */
+		tItemPtr temp = L->Active->nextItem; 		/* Stores address of item following active into variable */
+		L->Active->nextItem = temp->nextItem; 		/* Bridges active item and item behind soon-to-be deleted one */
 		free(temp);
 	}
 }
 
 void PostInsert (tList *L, tInst instr)
 {
+/** Inserts new item following active one
+* @param *L Linked list
+* @param instr Instruction
+*/
 	if (L->Active != NULL)
 	{
 		tItemPtr insert = NULL;
-		if ((insert = malloc(sizeof(struct tItem))) == NULL) /* V prípade nedostatku pamäte vracia NULL, volá ErrReport() a funkcia končí */
+		if ((insert = malloc(sizeof(struct tItem))) == NULL) /* Should allocation error occur, call ErrReport() func and quit */
 		{
 			ErrReport();
 			return;
 		}
 
-	insert->instruction = instr; 				/* Do dátovej časti prvku uloží hodnotu val */
-	insert->nextItem = L->Active->nextItem; 	/* Nový prvok bude ukazovať na prvok zoznamu za aktuálnym prvkom */
-	L->Active->nextItem = insert; 						/* Aktuálny prvok bude ukazovať na novo pridaný prvok */
+	insert->instruction = instr;
+	insert->nextItem = L->Active->nextItem; 	/* New item of the list will be pointing to the item beyond active one */
+	L->Active->nextItem = insert; 				/* Set active item's pointer to newly added item */
 	if (L->Active == L->Last)
 		L->Last = insert;
 	}
@@ -113,28 +143,43 @@ void PostInsert (tList *L, tInst instr)
 
 void Succ (tList *L)
 {
+/** Moves activity to the next item.
+* @param *L Linked list
+*/
 	if (L->Active == NULL)
 		return;
 
 	if (L->Active == L->Last)
-		L->Active = NULL;
+		L->Active = NULL;					/* If there's no more items in the list, null the activity */
 	else
-		L->Active = L->Active->nextItem; 	/* Posunie aktivitu na ďalší prvok, ak ďalší neexistuje zoznam sa stáva neakt. */
+		L->Active = L->Active->nextItem; 	/* Else, move activity to the next one */
 
 }
 
 void LGoto(tList *L, void *gotoInstr)
-{ /* Nastavi aktivny prvok podla adresy */
+{ 
+/** Sets active item using direct pointer
+* @param *L Linked list.
+* @param *gotoInstr Adress of required instruction
+*/
   L->Active = (tItemPtr) gotoInstr;
 }
 
 void *GetLastPointer(tList *L)
-{ /* Vrati pointer na posledny prvok */
+{
+/** Returns pointer to the last element of the list.
+* @param *L Linked List
+* @return Void pointer to the last element of the list
+*/ 
   return (void*) L->Last;
 }
 
 tInst *GetData(tList *L)
-{ /* Vrati aktivnu instrukciu */
+{
+/** Returns address of the active instruction
+* @param *L Linked List
+* @return Address of active instruction
+*/
   if (L->Active != NULL)
     return &(L->Active->instruction);
 
--- ilist.h
+++ ilist.h
@@ -2,16 +2,20 @@
  * Soubor:  ilist.h
  * Autor:   Adam Vass, xvassa00@stud.fit.vutbr.cz
  * Projekt: IFJ; zoznam instrukcii
+ *******************************************************************/
+/**
+*@file ilist.h
+*
+*@brief Header file for ilist.c
+*@author Adam Vass (xvassa00)
  */
 
 #ifndef _ILIST_H_
 #define _ILIST_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-
 typedef struct
 {
+/** Instruction structure, using three-address code */
 	int instructionType;
 	void *address1;
 	void *address2;
@@ -20,12 +24,14 @@ typedef struct
 
 typedef struct tItem
 {
+/** Individual list element */
   tInst instruction;
   struct tItem *nextItem;
 } *tItemPtr;
 
 typedef struct
 {
+/** Singly linked list */
   tItemPtr First;
   tItemPtr Last;
   tItemPtr Active;
